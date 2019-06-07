1a2583d0e0e847fbb97765fcdd1d007b40c765f8
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-06-07 14:48:55 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -13,7 +13,7 @@ class RepositoriesController < ApplicationController
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
-        @items = body["response"]["items"]
+        @items = body["items"]
       else
         @error = body["meta"]["errorDetail"]
       end
